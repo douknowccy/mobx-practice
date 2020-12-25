a6122a13caccfd5a37eb45f909aad7f81456baee
douknowccy <twkccy1990@gmail.com>
2020-12-25 19:50:47 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React from "react";
+import { NewNoteForm } from "./NewNoteForm";
+import { useNotesStore } from "./NotesContext";
+import { useObserver } from "mobx-react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const notesStore = useNotesStore();
+  return useObserver(() => {
+    console.log(notesStore.notes[0]);
+    return (
+      <>
+        <ul>
+          {notesStore.notes.map((note) => {
+            return (
+              <li onClick={() => notesStore.removeNote(note.id)} key={note.id}>
+                {note.text}
+              </li>
+            );
+          })}
+        </ul>
+        <NewNoteForm />
+      </>
+    );
+  });
 }
 
 export default App;
